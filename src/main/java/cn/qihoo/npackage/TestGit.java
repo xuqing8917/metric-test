05ea6f4faf9bb21d5d22a87c32529860baa039b6
xuqing <xuqing8917@163.com>
2017-06-22 10:34:07 +0800

    commit

---
+++ src/main/java/cn/qihoo/npackage/TestGit.java
@@ -0,0 +1,12 @@
+package cn.qihoo.npackage;
+
+
+/**
+ * Created by xuqing on 2017/6/22 0022.
+ */
+public class TestGit {
+
+    public static void main(String[] args){
+        System.out.println("success");
+    }
+}
